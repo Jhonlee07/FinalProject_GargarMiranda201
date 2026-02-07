40fb2e30f879cf1d0275a60b1a215357023fd908
Icey <iceyflame91@gmail.com>
2026-02-07 11:52:03 +0800

    notif/settings quickfix

--- app/src/main/java/com/example/finalss_gargarmiranda/SettingsActivity.kt
+++ app/src/main/java/com/example/finalss_gargarmiranda/SettingsActivity.kt
@@ -1,20 +1,15 @@
 package com.example.finalss_gargarmiranda
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        NavigationHelper.setupBottomNavigation(this, bottomNav, R.id.navigation_settings)
     }
 }
